f35f98f8f03c2be9f12088a648b1960ba2f62006
Scrimba-kelvin <kelvindonye353@gmail.com>
2024-12-20 06:38:00 +0100

    Initial commit

---
+++ Basketball.js
@@ -0,0 +1,33 @@
+let nmb = document.getElementById("number1")
+let addOne = 0
+function first() {
+  addOne = addOne + 1
+  nmb.innerText = addOne
+}
+
+function second() {
+  addOne = addOne + 2
+  nmb.innerText = addOne
+}
+
+function third() {
+  addOne = addOne + 3
+  nmb.innerText = addOne
+}
+
+let nmbe = document.getElementById("number2")
+let addOnet = 0
+function first2() {
+  addOnet = addOnet + 1
+  nmbe.innerText = addOnet
+}
+
+function second2() {
+  addOnet = addOnet + 2
+  nmbe.innerText = addOnet
+}
+
+function third2() {
+  addOnet = addOnet + 3
+  nmbe.innerText = addOnet
+}
